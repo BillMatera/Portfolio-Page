d7c4281f94be4898be44138ba0cf7518727080a9
BillMatera <matera.3@osu.edu>
2017-08-10 11:33:25 -0400

    try again

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+﻿import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdCheckboxModule } from '@angular/material';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+      BrowserModule,
+      BrowserAnimationsModule,
+      MdCheckboxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
