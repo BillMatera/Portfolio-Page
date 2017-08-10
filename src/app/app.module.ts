7b19fa8cfc571af754db48317af7f449c84b4689
BillMatera <matera.3@osu.edu>
2017-08-10 16:48:11 -0400

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 ﻿import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdCheckboxModule } from '@angular/material';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { MdCardModule } from '@angular/material';
+import { MdMenuModule } from '@angular/material';
+import { MdInputModule } from '@angular/material';
+import { MdButtonModule } from '@angular/material';
+import { MdListModule } from '@angular/material';
+import { MdIconModule } from '@angular/material';
+import { MdSidenavModule } from '@angular/material';
+import { MdToolbarModule } from '@angular/material';
+import { MdTooltipModule } from '@angular/material';
+import { MdDialogModule } from '@angular/material';
+import { MdTabsModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -11,7 +24,18 @@ import { AppComponent } from './app.component';
   imports: [
       BrowserModule,
       BrowserAnimationsModule,
-      MdCheckboxModule
+      CommonModule,
+      MdCardModule,
+      MdMenuModule,
+      MdInputModule,
+      MdButtonModule,
+      MdListModule,
+      MdIconModule,
+      MdSidenavModule,
+      MdToolbarModule,
+      MdTooltipModule,
+      MdDialogModule,
+      MdTabsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
